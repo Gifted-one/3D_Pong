290393d611288620a8401eae236b0f45d584c60e
Mpendulo <131270532+Gifted-one@users.noreply.github.com>
2025-04-08 23:49:57 +0200

    Further updates

--- Pong/Assets/Scripts/Ball_movement.cs
+++ Pong/Assets/Scripts/Ball_movement.cs
@@ -36,22 +36,29 @@ public class Ball_movement : MonoBehaviour
     {
         ToStart = P_racket.position - Ball.position;
 
-        if(transform.position.x >= 15 && transform.position.x <=18)
+        if(transform.position.x >= 30 && transform.position.x <=35)
         {
             AI.MoveRacket(CalculatePointOnX());
         }
 
+        if (transform.position.x < P_racket.transform.position.x - 5)
+        {
+            Prompt.OutComeScreen("Player");
+        }
+        else if(transform.position.x > AI_racket_rb.position.x + 5)
+        {
+            Prompt.OutComeScreen("Opponent");
+        }
 
-        
+
+
+
     }
 
     public void KickOff()
     {
         ToStart = P_racket.position - transform.position;
-        ToStart.y = 4;
         Ball.AddForce(ToStart.normalized*60f, ForceMode.VelocityChange);
-        //Ball.velocity = ToStart.normalized*30;
-        //Debug.Log("Changed Velocity");
     }
 
     public Vector3 CalculatePointOnX()
@@ -78,42 +85,31 @@ public class Ball_movement : MonoBehaviour
         return reaction_plane;
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "racket_P")
         {
-            Shoot(0);
-            //Debug.Log("Collided");
 
+            Target = new Vector3(0, 4, 0) - p_racket.transform.position;
+            if (Ball.position.y < 4)
+            {
+                Target.y = 2f;
+            }
+            Ball.velocity = Target*0.5f;
         }
         else if(collision.transform.tag == "racket_AI")
         {
             Prompt.ShouldKick = false;
-            Shoot(1);
-            
-            
+
+            Target = new Vector3(0, 4, 0) - AI_racket_rb.transform.position;
+            if(Ball.position.y < 4)
+            {
+                Target.y = 2f;
+            }
+            Ball.velocity = Target*0.5f;
         }
 
         
     }
 
-    void Shoot(int racket)
-    {
-        
-        if (racket == 0)
-        {
-            Target = new Vector3(0, 4, 0) - p_racket.transform.position;
-            
-        }
-        else if(racket == 1)
-        {
-            Target = AI_racket_rb.position  - new Vector3(0, 4, 0);
-            Target.y = 4f;
-            
-            
-        }
-
-        Ball.AddForce(Target*2, ForceMode.Impulse);
-
-    }
 }
--- Pong/Assets/Scripts/PlayerPrompt.cs
+++ Pong/Assets/Scripts/PlayerPrompt.cs
@@ -2,23 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 
 public class PlayerPrompt : MonoBehaviour
 {
 
     public TextMeshProUGUI Centre_text;
+    public TextMeshProUGUI Tutorial_text;
+    public TextMeshProUGUI Time_text;
+    public TextMeshProUGUI You_lostText;
 
     float Delay;
     float Timer = 0;
 
     public int countdown;
-    int TimeToStart = 5;
+    int TimeToStart = 30;
+    int TimetoDisplay = 0;
+    int inc = 0;
 
     bool ShouldFollow;
+    bool textDelay;
 
     public GameObject centreText;
     public GameObject ball;
+    public GameObject Pause_Screen;
+    public GameObject UnpauseButton;
 
     Ball_movement Ball;
     public bool ShouldKick;
@@ -30,39 +39,54 @@ public class PlayerPrompt : MonoBehaviour
         Ball = ball.GetComponent<Ball_movement>();
         ShouldFollow = true;
         ShouldKick = false;
+        textDelay = true;
+        Tutorial_text.gameObject.SetActive(false);
+        Pause_Screen.SetActive(false);
+        You_lostText.gameObject.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ShouldKick)
+        if(TimetoDisplay % 5 == 0 && textDelay)
         {
-            //Ball.KickOff();
+            tutorial_text();
+            Delay = 2;
+            textDelay = false;
         }
     }
     void FixedUpdate()
     {
         Timer += 1 * Time.deltaTime;
         countdown = (int)Timer;
-        
+        TimetoDisplay = (int)Timer;
 
-        if(TimeToStart > 0)
+        if(TimetoDisplay < 10)
         {
-            TimeToStart = 5;
+            Time_text.text = "Time: " + "0" + TimetoDisplay.ToString();
+
+        }
+        else
+        {
+            Time_text.text = "Time: " + TimetoDisplay.ToString();
+        }
+
+        if (TimeToStart > 0)
+        {
+            TimeToStart = 30;
             TimeToStart -= countdown;
             Centre_text.text = TimeToStart.ToString();
 
         }
         else if(TimeToStart == 0 && ShouldFollow)
         {
-            centreText.SetActive(false);
-            //ShouldKick = true;
+            Centre_text.gameObject.SetActive(false);
             Ball.KickOff();
             ShouldFollow = false;
         }
 
-        //Delay funtion
+        //Delay funtion, If needed.
         if(Delay > 0)
         {
             Delay -= Time.deltaTime;
@@ -70,7 +94,88 @@ public class PlayerPrompt : MonoBehaviour
         }
         else
         {
+            textDelay = true;
+        }
+    }
+
+    public void OutComeScreen(string who)
+    {
+        if(who == "Player")
+        {
+            Centre_text.gameObject.SetActive(true);
+            Centre_text.text = "You lose!";
+            Pause();
+            UnpauseButton.SetActive(false);
+            You_lostText.gameObject.SetActive(true);
+            You_lostText.text = "You lost.";
 
         }
+        else if(who == "Opponent")
+        {
+            Centre_text.gameObject.SetActive(true);
+            Centre_text.text = "You Win!";
+            Pause();
+            UnpauseButton.SetActive(false);
+            You_lostText.gameObject.SetActive(true);
+            You_lostText.text = "You won.";
+        }
+
+    }
+
+    void tutorial_text()
+    {
+        if (inc == 1)
+        {
+            Tutorial_text.gameObject.SetActive(true);
+            Tutorial_text.text = "Use your mouse to move the racket.";
+        }
+        else if (inc == 2)
+        {
+            Tutorial_text.text = "Press H to hide the cursor.";
+        }
+        else if (inc == 3)
+        {
+            Tutorial_text.text = "Use the scroll wheel to move the racket further or closer to the screen.";
+        }
+        else if (inc == 4)
+        {
+            Tutorial_text.text = "If the ball moves past you, you lose.";
+        }
+        else if (inc == 5)
+        {
+            Tutorial_text.text = "If it moves past your opponent, you win.";
+        }
+        else
+        {
+            Tutorial_text.gameObject.SetActive(false);
+        }
+        inc++;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Main");
+        
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        Pause_Screen.SetActive(true);
+        centreText.SetActive(false);
+
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+        Pause_Screen.SetActive(false);
+        centreText.SetActive(true);
     }
 }
